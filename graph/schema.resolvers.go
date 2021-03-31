3fb536b5ac69444dc519477e00fe61c48bd9c5ac
Renato Spakauskas <renatospaka@gmail.com>
2021-03-31 12:04:31 -0300

    feat: :sparkles: add resolvers to add course, chapter and category

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,6 +5,7 @@ package graph
 
 import (
 	"context"
+	"crypto/rand"
 	"fmt"
 
 	"github.com/renatospaka/fc2.0-graphql/graph/generated"
@@ -12,15 +13,51 @@ import (
 )
 
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
-	panic(fmt.Errorf("not implemented"))
+	category := model.Category{
+		ID:          fmt.Sprintf("T%d", rand.Int()),
+		Name:        input.Name,
+		Description: input.Description,
+	}
+
+	r.Categories = append(r.Categories, &category)
+	return &category, nil
 }
 
 func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCourse) (*model.Course, error) {
-	panic(fmt.Errorf("not implemented"))
+	var category *model.Category
+	for _, v := range r.Categories {
+		if v.ID == input.CategoryID {
+			category = v
+		}
+	}
+
+	course := model.Course{
+		ID:          fmt.Sprintf("T%d", rand.Int()),
+		Name:        input.Name,
+		Description: input.Description,
+		Category:    category,
+	}
+
+	r.Courses = append(r.Courses, &course)
+	return &course, nil
 }
 
 func (r *mutationResolver) CreateChapter(ctx context.Context, input model.NewChapter) (*model.Chapter, error) {
-	panic(fmt.Errorf("not implemented"))
+	var course *model.Course
+	for _, v := range r.Courses {
+		if v.ID == input.CourseID {
+			course = v
+		}
+	}
+
+	chapter := model.Chapter{
+		ID:          fmt.Sprintf("T%d", rand.Int()),
+		Name:        input.Name,		
+		Course:      course,
+	}
+
+	r.Chapters = append(r.Chapters, &chapter)
+	return &chapter, nil
 }
 
 func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
